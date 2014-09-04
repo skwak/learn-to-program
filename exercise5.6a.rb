bafae2a68be96191453ac035da4ef4e9f5abeb19
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-03 21:28:48 -0700

    Add the 1st exercise from Section 5.6

---
+++ exercise5.6a.rb
@@ -0,0 +1,8 @@
+puts "Why, hello. What's your first name, darlin'?"
+first_name = gets.chomp
+puts "That's a nice first name, " + first_name + ". What's your middle name?"
+middle_name = gets.chomp
+puts "Sounds good, " + first_name + " " + middle_name + ". What's your last name?"
+last_name = gets.chomp
+puts "A-ha! I finally have your full name, " + first_name + " " + middle_name +
+" " + last_name + "!"
