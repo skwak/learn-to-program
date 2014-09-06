64b9afaf752ead9b7fe49700381dcd72a8be45eb
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-06 13:44:51 -0700

    Added 99 bottles of beer

---
+++ exercise7.5-99bottles.rb
@@ -0,0 +1,10 @@
+bottles = 99
+
+while bottles >=2
+  puts "#{bottles} bottles of beer on the wall! #{bottles} bottles of beer!
+  Take one down and pass it around. #{bottles} bottles of beer on the wall!"
+  bottles -=1
+end
+
+puts "1 bottle of beer on the wall! 1 bottle of beer!
+Take one down and passit around. 1 bottle of beer on the wall!"
